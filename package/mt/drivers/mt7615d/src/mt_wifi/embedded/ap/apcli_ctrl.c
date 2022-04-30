76e9b4dc18d8d120bb61ac3a26b39c39b72febb7
Azexios <alexsdlb@gmail.com>
2022-04-30 04:06:14 +0000

    fix Makefile error mt7615 apcli WPA3 connect - always use SAE

--- package/mt/drivers/mt7615d/src/mt_wifi/embedded/ap/apcli_ctrl.c
+++ package/mt/drivers/mt7615d/src/mt_wifi/embedded/ap/apcli_ctrl.c
@@ -1397,7 +1397,7 @@ per mactable entry */
 				apcli_add_pmkid_cache(
 					pAd, pApCliEntry->MlmeAux.Bssid, pmkid,
 					pmk, LEN_PMK, ifIndex, CliIdx);
-				has_pmkid = TRUE;
+				has_pmkid = FALSE;
 			}
 
 			if (has_pmkid == TRUE) {
