24c65feee2cd4afdd55f5e11eda38c17639795d7
Azexios <alexsdlb@gmail.com>
2022-06-12 15:46:00 +1000

    add hostname and IPv4 display for the "Associated Stations"

--- package/mt/luci-app-mtwifi/root/usr/lib/lua/mtkwifis.lua
+++ package/mt/luci-app-mtwifi/root/usr/lib/lua/mtkwifis.lua
@@ -17,10 +17,17 @@ dme = mtkwifis.read_pipe("dmesg -c > /dev/null && iwpriv ra0 show stainfo 2>/dev
 os.execute("sleep 0.5")
 MAC = mtkwifis.read_pipe("dmesg | grep -oE '([A-Z0-9]{2}:){5}..' 2>/dev/null") or "?"
 RSSI = mtkwifis.read_pipe("dmesg | grep -oE '([-0-9]{3}\\/){3}...' 2>/dev/null") or "?"
-RSSI_M = mtkwifis.read_pipe("dmesg | grep -oE '([-0-9]{3}\\/){2}...' 2>/dev/null") or "?"
 BW = mtkwifis.read_pipe("dmesg | grep -oE '([0-9]{2,3}[A-Z]{1})\\/.{3,4}' 2>/dev/null" ) or "?"
 MCS = mtkwifis.read_pipe("dmesg | sed -nE '/([0-9]{2,3}[A-Z]{1})\\/.{3,4}/{n;p;}' | awk '{print $NF}' 2>/dev/null" ) or "?"
 SGI = mtkwifis.read_pipe("dmesg | sed -nE '/([0-9]{2,3}[A-Z]{1})\\/.{3,4}/{n;n;p;}' | awk '{print $NF}' 2>/dev/null" ) or "?"
-rate = mtkwifis.read_pipe("dmesg | awk '/0%/ {print a}{a=$NF}' 2>/dev/null" ) or "?"
+Rate = mtkwifis.read_pipe("dmesg | awk '/0%/ {print a}{a=$NF}' 2>/dev/null" ) or "?"
+
+for _,mac in ipairs(string.split(mtkwifis.read_pipe("dmesg | grep -oE '([A-Z0-9]{2}:){5}..'"), "\n"))
+do
+	os.execute("cat /tmp/dhcp.leases | grep -i '"..mac.."' | awk '{ print $4\" - \"$3 }' | grep '.*' >> /tmp/mtk/host || echo - >> /tmp/mtk/host")
+end
+
+Host = mtkwifis.read_pipe("cat /tmp/mtk/host 2>/dev/null") or "?"
+os.execute("rm -rf /tmp/mtk/host")
 
 return mtkwifis
