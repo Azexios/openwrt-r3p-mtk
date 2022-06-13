86bea976264923cfb41c8e001599e5f7b2cf7058
Alexey <66586711+Azexios@users.noreply.github.com>
2022-06-13 19:41:25 +1000

    fix "Associated Stations"

--- package/mt/luci-app-mtwifi/root/usr/lib/lua/mtkwifis.lua
+++ package/mt/luci-app-mtwifi/root/usr/lib/lua/mtkwifis.lua
@@ -1,7 +1,7 @@
 #!/usr/bin/env lua
 
 -- Associated Stations MTK Wi-Fi
--- For MT7615
+-- For MT7615 and driver version 5.1.0.0
 -- https://github.com/Azexios/openwrt-r3p-mtk
 
 local mtkwifis = {}
@@ -16,10 +16,10 @@ end
 dme = mtkwifis.read_pipe("dmesg -c > /dev/null && iwpriv ra0 show stainfo 2>/dev/null & iwpriv rai0 show stainfo 2>/dev/null")
 os.execute("sleep 0.5")
 MAC = mtkwifis.read_pipe("dmesg | grep -oE '([A-Z0-9]{2}:){5}..' 2>/dev/null") or "?"
-RSSI = mtkwifis.read_pipe("dmesg | grep -oE '([-0-9]{3}\\/){3}...' 2>/dev/null") or "?"
-BW = mtkwifis.read_pipe("dmesg | grep -oE '([0-9]{2,3}[A-Z]{1})\\/.{3,4}' 2>/dev/null" ) or "?"
-MCS = mtkwifis.read_pipe("dmesg | sed -nE '/([0-9]{2,3}[A-Z]{1})\\/.{3,4}/{n;p;}' | awk '{print $NF}' 2>/dev/null" ) or "?"
-SGI = mtkwifis.read_pipe("dmesg | sed -nE '/([0-9]{2,3}[A-Z]{1})\\/.{3,4}/{n;n;p;}' | awk '{print $NF}' 2>/dev/null" ) or "?"
+RSSI = mtkwifis.read_pipe("dmesg | grep -oE '([-0-9]{3,4}\\/){3}.{3,4}' 2>/dev/null") or "?"
+BW = mtkwifis.read_pipe("dmesg | grep -oE '([0-9]{2,3}[A-Z])\\/.{3,4}' 2>/dev/null" ) or "?"
+MCS = mtkwifis.read_pipe("dmesg | sed -nE '/([0-9]{2,3}[A-Z])\\/.{3,4}/{n;p;}' | awk '{print $NF}' 2>/dev/null" ) or "?"
+SGI = mtkwifis.read_pipe("dmesg | sed -nE '/([0-9]{2,3}[A-Z])\\/.{3,4}/{n;n;p;}' | awk '{print $NF}' 2>/dev/null" ) or "?"
 Rate = mtkwifis.read_pipe("dmesg | awk '/0%/ {print a}{a=$NF}' 2>/dev/null" ) or "?"
 
 for _,mac in ipairs(string.split(mtkwifis.read_pipe("dmesg | grep -oE '([A-Z0-9]{2}:){5}..'"), "\n"))
